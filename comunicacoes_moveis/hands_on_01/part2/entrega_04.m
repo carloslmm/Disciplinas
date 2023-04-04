bd6e66acabda1753e1c56a34a51fee96f626cc79
Your Name <you@example.com>
2023-04-03 23:14:52 -0300

    Terminando entrega 04

--- comunicacoes_moveis/hands_on_01/part2/entrega_04.m
+++ comunicacoes_moveis/hands_on_01/part2/entrega_04.m
@@ -1,86 +1,10 @@
-% Entrada de parâmetros
-dR = 500;                                                  % Raio do Hexágono
-dFc = 800;                                                 % Frequência da portadora
-dSigmaShad = 8;                                            % Desvio padrão do sombreamento lognormal
-% Cálculos de outras variáveis que dependem dos parâmetros de entrada
-dPasso = ceil(dR/20);                                      % Resolução do grid: distância entre pontos de medição
-dRMin = dPasso;                                            % Raio de segurança
-dIntersiteDistance = 2*sqrt(3/4)*dR;                       % Distância entre ERBs (somente para informação)
-dDimX = 5*dR;                                              % Dimensão X do grid
-dDimY = 6*sqrt(3/4)*dR;                                    % Dimensão Y do grid
-dPtdBm = 21;                                               % EIRP (incluindo ganho e perdas) (https://pt.slideshare.net/naveenjakhar12/gsm-link-budget)
-dPtLinear = 10^(dPtdBm/10)*1e-3;                           % EIRP em escala linear
-dHMob = 1.5;                                                 % Altura do receptor
-dHBs = 32;                                                 % Altura do transmissor
-dAhm = 3.2*(log10(11.75*dHMob)).^2 - 4.97;                 % Modelo Okumura-Hata: Cidade grande e fc  >= 400MHz
-% Vetor com posições das BSs (grid Hexagonal com 7 células, uma célula central e uma camada de células ao redor)
-vtBs = [ 0 ];
-dOffset = pi/6;
-for iBs = 2 : 7
-    vtBs = [ vtBs dR*sqrt(3)*exp( j * ( (iBs-2)*pi/3 + dOffset ) ) ];
+Frq = [800 1800 2100];     % Frequência da Portadora
+R = 500;                    % Raio do Hexágono
+scenario = 0;                % Valores: 0 (Macrocélula), 1 (Microcélula)
+for i = 1:length(Frq)
+    fDrawREM(Frq(i),R, scenario);       % REM usando somente macrocélula
 end
-vtBs = vtBs + (dDimX/2 + j*dDimY/2);                        % Ajuste de posição das bases (posição relativa ao canto inferior esquerdo)
-%
-% Matriz de referência com posição de cada ponto do grid (posição relativa ao canto inferior esquerdo)
-dDimY = ceil(dDimY+mod(dDimY,dPasso));                      % Ajuste de dimensão para medir toda a dimensão do grid
-dDimX = ceil(dDimX+mod(dDimX,dPasso));                      % Ajuste de dimensão para medir toda a dimensão do grid
-[mtPosx,mtPosy] = meshgrid(0:dPasso:dDimX, 0:dPasso:dDimY);
-% Iniciação da Matriz de com a pontência de recebida máxima em cada ponto
-% medido. Essa potência é a maior entre as 7 ERBs.
-mtPowerFinaldBm = -inf*ones(size(mtPosy));
-% Calcular O REM de cada ERB e aculumar a maior potência em cada ponto de medição
-for iBsD = 1 : length(vtBs)                                 % Loop nas 7 ERBs
-    % Matriz 3D com os pontos de medição de cada ERB. Os pontos são
-    % modelados como números complexos X +jY, sendo X a posição na abcissa e Y, a posição no eixo das ordenadas
-    mtPosEachBS = (mtPosx + j*mtPosy)-(vtBs(iBsD));
-    mtDistEachBs = abs(mtPosEachBS);              % Distância entre cada ponto de medição e a sua ERB
-    mtDistEachBs(mtDistEachBs < dRMin) = dRMin;             % Implementação do raio de segurança
-    % Okumura-Hata (cidade urbana) - dB
-    mtPldB = 69.55 + 26.16*log10(dFc) + (44.9 - 6.55*log10(dHBs))*log10(mtDistEachBs/1e3) - 13.82*log10(dHBs) - dAhm;
-    % Potências recebidas em cada ponto de medição sem shadowing
-    mtPowerEachBSdBm = dPtdBm - mtPldB;                   
-    % Cálulo da maior potência em cada ponto de medição sem shadowing
-    mtPowerFinaldBm = max(mtPowerFinaldBm,mtPowerEachBSdBm);
-    
+scenario = 1;
+for i = 1:length(Frq)
+    fDrawREM(Frq(i),R, scenario);       % REM usando macro e micro células
 end
-
-m_size = size(mtPowerFinaldBm);
-mtOut = [];             % Matriz para área de Outage
-
-for i = 1:m_size(1)
-    for j = 1:m_size(2)
-        if mtPowerFinaldBm(i,j) > dPtdBm
-            mtOut(i,j) = 100; 
-        else
-            mtOut(i,j) = -100;
-        end
-    end
-end
-
-%Plot da REM de todo o grid (composição das 7 ERBs) sem shadowing
-figure;
-pcolor(mtPosx,mtPosy,mtOut);
-colormap(autumn);
-colorbar;
-fDrawDeploy(dR,vtBs);
-axis equal;
-title(['Teste']);
-%{ 
-%Plot da REM de todo o grid (composição das 7 ERBs) sem shadowing
-figure;
-pcolor(mtPosx,mtPosy,mtPowerFinaldBm);
-colormap(hsv);
-colorbar;
-fDrawDeploy(dR,vtBs);
-axis equal;
-title(['Todas as 7 ERB sem shadowing']);
-%
-% Plot da REM de todo o grid (composição das 7 ERBs) sem shadowing
-figure;
-pcolor(mtPosx,mtPosy,mtPowerFinalShaddBm);
-colormap(hsv);
-colorbar;
-fDrawDeploy(dR,vtBs);
-axis equal;
-title(['Todas as 7 ERB com shadowing']);
-%}
---
+++ comunicacoes_moveis/hands_on_01/part2/fDrawREM.m
@@ -0,0 +1,103 @@
+function fDrawREM(dFc, dR, scenario)
+    % Entrada de parâmetros
+    dSigmaShad = 8;                                            % Desvio padrão do sombreamento lognormal
+    % Cálculos de outras variáveis que dependem dos parâmetros de entrada
+    dPasso = ceil(dR/20);                                      % Resolução do grid: distância entre pontos de medição
+    dRMin = dPasso;                                            % Raio de segurança
+    dIntersiteDistance = 2*sqrt(3/4)*dR;                       % Distância entre ERBs (somente para informação)
+    dDimX = 5*dR;                                              % Dimensão X do grid
+    dDimY = 6*sqrt(3/4)*dR;                                    % Dimensão Y do grid
+    dPtdBmMicro = 20;
+    dPtdBmMacro = 21;                                               % EIRP (incluindo ganho e perdas) (https://pt.slideshare.net/naveenjakhar12/gsm-link-budget)
+    dPtLinear = 10^(dPtdBmMacro/10)*1e-3;                           % EIRP em escala linear
+    dHMob = 1.5;                                                 % Altura do receptor
+    dHBs = 32;                                                 % Altura do transmissor
+    
+    % Matriz de referência com posição de cada ponto do grid (posição relativa ao canto inferior esquerdo)
+    dDimY = ceil(dDimY+mod(dDimY,dPasso));                      % Ajuste de dimensão para medir toda a dimensão do grid
+    dDimX = ceil(dDimX+mod(dDimX,dPasso));                      % Ajuste de dimensão para medir toda a dimensão do grid
+    [mtPosx,mtPosy] = meshgrid(0:dPasso:dDimX, 0:dPasso:dDimY);
+    % Iniciação da Matriz de com a pontência de recebida máxima em cada ponto
+    % medido. Essa potência é a maior entre as 7 ERBs.
+    mtPowerFinaldBm = -inf*ones(size(mtPosy));
+    % Vetor com posições das BSs na macrocélula
+    vtBsMacro = [ 0 ];
+    dOffset = pi/6;
+    for iBs = 2 : 7
+        vtBsMacro = [ vtBsMacro dR*sqrt(3)*exp( j * ( (iBs-2)*pi/3 + dOffset ) ) ];
+    end
+    vtBsMacro = vtBsMacro + (dDimX/2 + j*dDimY/2);                        % Ajuste de posição das bases (posição relativa ao canto inferior esquerdo)
+    % Vetor com posições das BSs na macrocélula
+    vtBsMicro = [1000+j*1732 1500+j*1732 1750+j*1299 1500+j*866 1000+j*866 750+j*1299];
+    % Calcular O REM de cada ERB e aculumar a maior potência em cada ponto de medição
+    for iBsD = 1 : length(vtBsMacro)                                 % Loop nas 7 ERBs
+        % Matriz 3D com os pontos de medição de cada ERB. Os pontos são
+        % modelados como números complexos X +jY, sendo X a posição na abcissa e Y, a posição no eixo das ordenadas
+        mtPosEachBS = (mtPosx + j*mtPosy)-(vtBsMacro(iBsD));
+        mtDistEachBs = abs(mtPosEachBS);              % Distância entre cada ponto de medição e a sua ERB
+        mtDistEachBs(mtDistEachBs < dRMin) = dRMin;             % Implementação do raio de segurança
+        % Perda de percurso
+        mtPldB = 55 + 38*log10(mtDistEachBs/1e3) + (24.5 + (1.5*dFc)/925)*log10(dFc);
+        % Potências recebidas em cada ponto de medição sem shadowing
+        mtPowerEachBSdBm = dPtdBmMacro - mtPldB;                   
+        % Cálulo da maior potência em cada ponto de medição sem shadowing
+        mtPowerFinaldBm = max(mtPowerFinaldBm,mtPowerEachBSdBm);    
+    end
+    m_size = size(mtPowerFinaldBm);
+    mtOut = [];             % Matriz para área de Outage
+    for i = 1:m_size(1)
+        for k = 1:m_size(2)
+            if mtPowerFinaldBm(i,k) >= -90
+                mtOut(i,k) = 100; 
+            else
+                mtOut(i,k) = -100;
+            end
+        end
+    end
+    
+    if scenario == 1
+        % Calcular O REM de cada ERB e aculumar a maior potência em cada ponto de medição
+        for iBsD = 1 : length(vtBsMicro)                                 % Loop nas 7 ERBs
+            % Matriz 3D com os pontos de medição de cada ERB. Os pontos são
+            % modelados como números complexos X +jY, sendo X a posição na abcissa e Y, a posição no eixo das ordenadas
+            mtPosEachBS = (mtPosx + j*mtPosy)-(vtBsMicro(iBsD));
+            mtDistEachBs = abs(mtPosEachBS);              % Distância entre cada ponto de medição e a sua ERB
+            mtDistEachBs(mtDistEachBs < dRMin) = dRMin;             % Implementação do raio de segurança
+            % Perda de percurso
+            mtPldB = 55 + 38*log10(mtDistEachBs/1e3) + (24.5 + (1.5*dFc)/925)*log10(dFc);
+            % Potências recebidas em cada ponto de medição sem shadowing
+            mtPowerEachBSdBm = dPtdBmMicro - mtPldB;                   
+            % Cálulo da maior potência em cada ponto de medição sem shadowing
+            mtPowerFinaldBm = max(mtPowerFinaldBm,mtPowerEachBSdBm);    
+        end
+        m_size = size(mtPowerFinaldBm);
+        mtOut = [];             % Matriz para área de Outage
+        for i = 1:m_size(1)
+            for k = 1:m_size(2)
+                if mtPowerFinaldBm(i,k) >= -90
+                    mtOut(i,k) = 100; 
+                else
+                    mtOut(i,k) = -100;
+                end
+            end
+        end
+    end
+    if scenario == 0
+        %Plot da REM de todo o grid (composição das 7 ERBs) sem shadowing
+        figure;
+        pcolor(mtPosx,mtPosy,mtOut);
+        colormap(autumn);
+        colorbar;
+        fDrawDeploy(dR,vtBsMacro);
+        axis equal;
+        title(['Macrocélula para frequência de ' num2str(dFc) 'MHz']);
+    elseif scenario == 1
+        %Plot da REM de todo o grid (composição das 7 ERBs) sem shadowing
+        figure;
+        pcolor(mtPosx,mtPosy,mtOut);
+        colormap(autumn);
+        fDrawDeploy(dR,vtBsMacro);
+        axis equal;
+        title(['Macrocélulas e microcélulas para frequência de ' num2str(dFc) 'MHz']);
+    end
+end
