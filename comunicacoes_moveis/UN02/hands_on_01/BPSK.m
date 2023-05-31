10f28b0cfc28b121c6f23f6759b387603cfbb8b8
Your Name <you@example.com>
2023-05-31 11:48:24 -0300

    Up

--- comunicacoes_moveis/UN02/hands_on_01/BPSK.m
+++ comunicacoes_moveis/UN02/hands_on_01/BPSK.m
@@ -1,10 +1,12 @@
 clear all; clc;close all
-Nfft=64; msg = 100; %n ́umero total de subportadoras
-Na = Nfft*msg; %Tamanho da mensagem (em bits)
-a = randi([01], 1, Na);
-SM = [-1 +1];
-amt = reshape(a, msg, Nfft); %Simbolo transmitido pelas subportadoras
-Vn = SM(a + 1); %
+
+N=100000;
+d=rand(1,N)>0.5; %data generation
+X=2*d-1; %BPSK modulation
+
+%Sinal OFDM
+ofdmsym_sp = ifft(reshape(X,length(X)/50, 50));
+xn = reshape(ofdmsym_sp,1,length(X));
 
 EbN0dB = 0:1:14;
 
@@ -12,27 +14,39 @@ Eb = 1;
 
 sigmas= zeros(0,length(EbN0dB));             % Vetor de variâncias do ruído
 for i = 1:length(EbN0dB)
-    N0 = Eb/10^(EbN0dB(i)/10);
+    N0 = Eb*10^(-EbN0dB(i)/10);
     sigmas(i) = N0/2;
 end
 
-Vntime = ifft(Vn);
-vtvntime = reshape (Vntime, 1,Na);
-
 BER_TEO = zeros(0,length(EbN0dB));
 for ik = 1:length(sigmas)
-    %
-    % Adição de ruído
-    variance = sigmas(ik);
-    noise = sqrt(variance)*[1 j] * randn(2, length(vtvntime));
-    rt = vtvntime + noise;
-    Rtfreq = fft(rt);
+    for jk = 1:500
+        %
+        % Adição de ruído
+        variance = sigmas(ik);
+        noise = sqrt(variance)*randn(1,N)+1i*sqrt(variance)*randn(1,N);
+
+        rn = xn + noise;
+
+        Rt_freq = fft(reshape(rn,length(rn)/(50),50));
+        Y = reshape(Rt_freq,1,length(xn));
+
+        %Demodulação BPSK 
+
+        bits_recebidos = sign(real(Y));
+
+        %BER Simulado
+        BER_SIM(jk) = sum(bits_recebidos ~= X)/length(X);
+    end
     %BER teórico
-    BER_TEO(ik) = 0.5*erfc(sqrt(10^(EbN0dB(ik)/10)));   
+    BER_TEO(ik) = 0.5*erfc(sqrt(10^(EbN0dB(ik)/10)));
+    %BER simulado
+    BER_SIM_MC(ik) = mean(BER_SIM);
 end
 
 semilogy(EbN0dB,BER_TEO,'r-');
 hold on;
-legend('BPSK-BER Teórico','BPSK- BER SImulado');
+semilogy(EbN0dB,BER_SIM_MC,'b*');
+legend('BPSK-BER Teórico','BPSK- BER Simulado');
 xlabel('Eb/N0(dB)');
-ylabel('Symbol Error Rate (Ps)');
+ylabel('Bit Error Rate (BER)');
